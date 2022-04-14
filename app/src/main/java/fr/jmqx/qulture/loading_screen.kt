37d8623a70711b7fcd0835584cb015dd7d6659ee
Michel <michel.kaminski@efrei.net>
2022-04-14 09:50:43 +0200

    PAGE DE DEMARRAGE(sans barre de chargement)

---
+++ app/src/main/java/fr/jmqx/qulture/loading_screen.kt
@@ -0,0 +1,14 @@
+package fr.jmqx.qulture
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class loading_screen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_loading_screen)
+
+
+    }
+
+}
